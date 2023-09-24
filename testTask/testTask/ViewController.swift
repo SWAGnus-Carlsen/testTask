e301bf42481957314f286528c780d27ec0a615d5
SWAGnus-Carlsen <92265364+SWAGnus-Carlsen@users.noreply.github.com>
2023-09-24 14:17:58 +0300

    project created

---
+++ testTask/testTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  testTask
+//
+//  Created by Vitaliy Halai on 24.09.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
